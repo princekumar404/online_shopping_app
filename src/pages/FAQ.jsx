b91c1c5fa24891fc32b2fbfb1d294f89680c3c32
princekumar404 <princekumar07847@gmail.com>
2026-04-20 18:18:16 +0530

    Create FAQ.jsx

---
+++ src/pages/FAQ.jsx
@@ -0,0 +1,13 @@
+function Privacy() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Privacy Policy</h1>
+      <p>
+        This website respects user privacy. No personal data is shared.
+        All information is stored locally for demonstration purposes.
+      </p>
+    </div>
+  );
+}
+
+export default Privacy;
